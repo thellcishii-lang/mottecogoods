eb78aaaf24f373acd1b089e496ce92cb4433512d
thellcishii-lang <the.llcishii@gmail.com>
2026-09-26 10:22:14 +0900

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 const navLinks = [
-  const navLinks = [
   { href: "/", label: "ホーム" },
   { href: "/products", label: "商品一覧" },
   { href: "/about", label: "会社概要" },
